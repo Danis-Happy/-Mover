8bd22b6e9438485fe58018a272831933d70e16b9
lito2000 <ihsoft@IHSOFTui-MacBook-Pro.local>
2015-05-15 14:50:08 +0900

    Initial Commit

---
+++ Mover/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Mover
+//
+//  Created by IHSOFT on 2015. 5. 15..
+//  Copyright (c) 2015년 IHSOFT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
